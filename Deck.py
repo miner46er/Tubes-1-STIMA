f5d12c3e7f3ae48d1e13b7da6a9f50bdf52eff6f
Eka Novendra Wahyunadi <ekanovendra726@gmail.com>
2019-02-10 14:44:50 +0700

    GUI done

--- Deck.py
+++ Deck.py
@@ -5,13 +5,15 @@ class Deck:
 
     def __init__(self):
         '''
+        Menginisialisasi kelas Deck dengan atribut cards yang berisi list of tuple.
+        Tuple tersebut berisi file path gambar kartu dan nilai dari setiap kartu.
         '''
         __cardNameModifier = ['c', 'd', 'h', 's']
-        
+
         imgDir = os.path.join(os.path.dirname(__file__), 'img')
-        
+
         self.cards = []
-        
+
         for i in range(1, 14):
                 for modifier in __cardNameModifier:
                     fileName = str(i) + modifier  + '.png'
@@ -19,13 +21,13 @@ class Deck:
 
     def getCardsCount(self):
         return len(self.cards)
-    
+
     def randomizeDeck(self):
         '''
         Mengacak list berisi path ke.
         '''
         random.shuffle(self.cards)
-    
+
     def drawCards(self, x = 1):
         '''
         Returns
---
+++ main.py
@@ -0,0 +1,31 @@
+import kivy
+import Deck
+import algoritma_greedy as algo
+kivy.require("1.9.0")
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+class SolverGridLayout(GridLayout):
+
+    deck = Deck.Deck()
+    showncards=[['',''],['',''],['',''],['','']]
+    count=0
+    deck_blank=['./img/back.png','./img/back.png','./img/back.png','./img/back.png','./img/back.png','./img/back.png','./img/back.png','./img/back.png','./img/back.png','./img/back.png','./img/back.png','./img/back.png','./img/back.png','./img/blank.png']
+
+    def solve(self):
+        return algo.solve24_greedy([str(card[1]) for card in self.showncards]);
+    def shuffle(self):
+        self.deck.randomizeDeck()
+    def draw(self):
+        if self.count<13:
+            self.showncards = self.deck.drawCards(4)
+            self.count=self.count+1
+
+class SolverApp(App):
+
+    def build(self):
+        return SolverGridLayout()
+
+solveApp = SolverApp()
+solveApp.run()
